80f9891a367ae42b36ecbf5883ac2803e94dbb7e
miaowing <me@zf.ink>
2020-09-17 18:27:07 +0800

    update etcd options

--- packages/etcd/interfaces/etcd-options.interface.ts
+++ packages/etcd/interfaces/etcd-options.interface.ts
@@ -1,18 +1,26 @@
+import { ChannelOptions } from '@grpc/grpc-js/build/src/channel-options';
+import { CallOptions } from '@grpc/grpc-js';
+import { IPolicy, IBackoff } from 'cockatiel';
+
 export interface EtcdOptions {
     credentials?: {
         rootCertificate: Buffer;
         privateKey?: Buffer;
         certChain?: Buffer;
     };
-    grpcOptions?: any;
+    grpcOptions?: ChannelOptions;
     auth?: {
         username: string;
         password: string;
+        callOptions?: CallOptions;
     };
     hosts?: string[] | string;
     dialTimeout?: number;
-    backoffStrategy?: any;
-    retry?: boolean;
+    faultHandling?: Partial<{
+        host: (hostname: string) => IPolicy<any>;
+        global: IPolicy<any>;
+        watchBackoff: IBackoff<unknown>;
+    }>;
 }
 
 export interface AsyncEtcdOptions {
