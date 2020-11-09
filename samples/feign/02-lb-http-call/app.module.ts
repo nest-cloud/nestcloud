57f6709159ea79eec940d24b8bd2c05592cddcef
Andrew Miracle <me@andrewmiracle.com>
2020-11-09 09:45:32 +0000

    add forRootAsync to feign module

--- samples/feign/02-lb-http-call/app.module.ts
+++ samples/feign/02-lb-http-call/app.module.ts
@@ -3,7 +3,7 @@ import { BootModule } from '@nestcloud/boot';
 import { ConsulModule } from '@nestcloud/consul';
 import { ConsulServiceModule } from '@nestcloud/consul-service';
 import { FeignModule } from '@nestcloud/feign';
-import { BOOT, NEST_CONSUL_LOADBALANCE } from '@nestcloud/common';
+import { BOOT, LOADBALANCE } from '@nestcloud/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpClient } from "./http.client";
 import { TestService } from "./test.service";
@@ -15,9 +15,9 @@ import { resolve } from 'path'
         BootModule.forRoot({
             filePath: resolve(__dirname, `config.yaml`)
         }),
-        ConsulModule.register({ inject: [BOOT] }),
-        ConsulServiceModule.register({ inject: [BOOT] }),
-        FeignModule.register({ inject: [BOOT, NEST_CONSUL_LOADBALANCE] }),
+        ConsulModule.forRootAsync({ inject: [BOOT] }),
+        ConsulServiceModule.forRootAsync({ inject: [BOOT] }),
+        FeignModule.forRootAsync({ inject: [BOOT, LOADBALANCE] }),
         TerminusModule.forRootAsync({
             useFactory: () => ({ endpoints: [{ url: '/health', healthIndicators: [] }] }),
         }),
